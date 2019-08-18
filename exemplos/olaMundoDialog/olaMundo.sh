91c080949934c57a21bf805c0963d722b01f0a60
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-08-18 15:32:25 -0300

    Exemplo, olá mundo dialog

---
+++ exemplos/olaMundoDialog/olaMundo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# olaMundo.sh (Shell Script)
+# 
+# Objetivo: Exemplo de utilização do programa Dialog - Olá mundo!
+# 
+# Site: http://www.dirackslounge.online
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo Dirack 18/08/2019
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+
+dialog --msgbox 'Olá mundo dialog' 5 40
