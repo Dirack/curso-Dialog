26537f5f7f94fdbbcbd3ef36d8733361a00adcc7
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-09-15 21:09:19 -0300

    Exemplo de uso checklist do Dialog

---
+++ exemplos/checklist/exemplo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# exemplo.sh (Shell Script)
+# 
+# Objetivo: Exemplo de uso checklist do Dialog.
+# 
+# Site: http://www.dirackslounge.online
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo Dirack 15/09/2019
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+TAREFAS=$(dialog --stdout \
+	--title "Lista de tarefas" \
+	--checklist "Marque as tarefas finalizadas:" \
+	0 0 0 \
+	"Lavar a louça" "1" on \
+	"Arrumar a cama" "2" off \
+	"Dar banho no cachorro" "3" on)
+
+echo "Vc terminou as seguintes tarefas:"
+echo "$TAREFAS"
